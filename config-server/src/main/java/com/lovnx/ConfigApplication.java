0a986bff7ae0be29fbfc506e7f04460a26585354
正义之剑 <yuhaibao324@126.com>
2019-02-09 06:56:04 +0800

    把configserver添加到注册中心

--- config-server/src/main/java/com/lovnx/ConfigApplication.java
+++ config-server/src/main/java/com/lovnx/ConfigApplication.java
@@ -2,8 +2,10 @@ package com.lovnx;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
